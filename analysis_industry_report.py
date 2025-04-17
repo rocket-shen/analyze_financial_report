e6322e8145a3b1d3ea5fae25ee4515d4597416c9
rocket-shen <rocket.shen@gmail.com>
2025-04-17 15:29:16 +0800

    Update analysis_industry_report.py

--- analysis_industry_report.py
+++ analysis_industry_report.py
@@ -26,13 +26,18 @@ def analysis_data(f):
     df_analysis["净利润增长率"] = ((df_income["归属于母公司股东的净利润"] / df_income["归属于母公司股东的净利润"].shift(-4)) - 1) * 100
     df_analysis["营收增长率"] = ((df_income["其中：营业收入"] / df_income["其中：营业收入"].shift(-4)) - 1) * 100
     df_analysis["资产负债率"] = df_balance["负债合计"] / df_balance["负债和股东权益总计"]
-    df_selected = df_analysis[["报告期", "ROE","净利率", "毛利率", "营收增长率", "净利润增长率", "资产负债率"]].iloc[:20]
+    df_selected = df_analysis[["报告期", "ROE","净利率", "毛利率", "营收增长率", "净利润增长率", "资产负债率"]]
 
     # 将 "报告期" 列设置为索引
     df_selected.set_index('报告期', inplace=True)
-
+    df_selected = df_selected.loc[(df_selected.index >= '2018-03-31') & (df_selected.index <= '2025-03-31')]
     # 行列翻转（让 "报告期" 变成列索引）
     result = df_selected.T
+
+    # 对列名（报告期）进行排序，按日期从早到晚
+    result.columns = pd.to_datetime(result.columns)  # 将报告期转换为日期格式
+    result = result[sorted(result.columns)]  # 按日期排序
+    result.columns = result.columns.strftime('%Y/%m/%d')  # 转换回字符串格式
     return result
 
 directory = r"D:\雪球数据\行业财务报表"  # 注意：路径中的反斜杠需要转义，或者使用原始字符串（前面加r）
